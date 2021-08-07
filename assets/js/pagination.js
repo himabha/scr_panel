9e1203bab7f0adbd3addca5578d01a68d1426127
himbha9 <himanshu.bhatia9@gmail.com>
2021-08-07 16:29:08 +0530

    Added pagination for previous page

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -18,6 +18,22 @@ $(document).ready(function(){
 	pagination += "<a id='nextpage' href='javascript:nextpage()'><p id='next'>Next</p></a>";
 	$("#pagination").html(pagination);
 	$("#page_1").addClass("active");
+	//page1
+	var pagechild = $("#pagination").children();
+
+	if ($('#page_' + (pagechild.length - 2)).css('display') == 'none') {
+		$("#next").prepend('<span>......</span>');
+	}
+	for (var i = 1; i < 6; i++) {
+		$("#page_" + i).css("display", "block");
+	}
+	for (var i = 6; i < pagechild.length; i++) {
+		$("#page_" + i).css("display", "none");
+	}
+
+	if ($('#page_' + (pagechild.length - 2)).css('display') == 'none') {
+		$("#next").prepend("<span id='nextspan'>............</span>");
+	}
 
 });
 
@@ -56,4 +72,44 @@ function gotopage(pageno) {
         $("#prev").append("<span id='prevspan'>...........</span>");
     }
 
+}
+
+function prevpage() {
+    if ($("#page_1").hasClass("active") != true) {
+        $("#page").children().css("display", "none");
+        var abc = $(".active").attr("id").split("_");
+        var start = (parseInt(abc[1]) - 2) * pagelimit;
+        var end = (parseInt(abc[1]) - 2) * pagelimit + pagelimit;
+        for (var l = start; l < end; l++) {
+
+            $("#page").children().eq(l).css("display", "block");
+        }
+        var p_no = parseInt(abc[1]) - 1;
+        $("#page_" + p_no).addClass("active");
+        $("#page_" + p_no).siblings().removeClass("active");
+    }
+    //page3
+    var pagechild = $("#pagination").children();
+    for (var i = p_no; i < (p_no + 5); i++) {
+        $("#page_" + i).css("display", "block");
+    }
+    for (var i = 1; i < p_no; i++) {
+        $("#page_" + i).css("display", "none");
+    }
+    for (var i = p_no + 6; i < pagechild.length; i++) {
+        $("#page_" + i).css("display", "none");
+    }
+    console.log($('#page_' + (pagechild.length - 2)).css('display'));
+    if ($('#page_' + (pagechild.length - 2)).css('display') == 'block') {
+        $("span").remove();
+    } else {
+        $("span").remove();
+        $("#next").prepend("<span id='nextspan'>............</span>");
+    }
+    if ($('#page_1').css('display') == 'block') {
+        $("#prevspan").remove();
+    } else {
+        $("#prevspan").remove();
+        $("#prev").append("<span id='prevspan'>...........</span>");
+    }
 }
